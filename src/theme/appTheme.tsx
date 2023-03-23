963672407a61f81e36bac569f569979b32258a71
Alejo Gomez <alejandro.gomezq97@gmail.com>
2023-03-22 19:06:57 -0500

    feat: created first button structure

--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -5,8 +5,33 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#000000',
   },
-  text: {
+  calculatorContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+  },
+  resultMemory: {
+    textAlign: 'right',
+    color: 'rgba(255, 255, 255, 0.5)',
+    fontSize: 30,
+  },
+  result: {
+    textAlign: 'right',
     color: '#ffffff',
-    fontSize: 20,
+    fontSize: 60,
+  },
+  btn: {
+    justifyContent: 'center',
+    height: 80,
+    width: 80,
+    borderRadius: 100,
+    backgroundColor: '#9b9b9b',
+  },
+  textBtn: {
+    textAlign: 'center',
+    padding: 10,
+    color: '#000000',
+    fontSize: 30,
+    fontWeight: '400',
   },
 });
